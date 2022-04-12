7aff850d89fe93d6d945751d33eeb3a4571a810d
Jessica Edmonds <jessica1329b@gmail.com>
2022-04-12 17:15:15 -0400

    palindrome exercise

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,34 @@
 using System;
 using Xunit;
+using ExampleForPalindrome;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+       [Theory]
+       [InlineData("racecar", true)]
+       [InlineData("hello", false)]
+        [InlineData("Racecar", true)]
+        public void MyTest(string.word, bool expected) 
         {
+            //arrange
+            var tester = new WordSmith();
+
+            //act
+            bool actual = tester.IsAPalindrome(word);
+
+            //assert
+            Assert.Equal(expected, actual);
+
+
+
 
         }
+
+
+
+
+
     }
 }
